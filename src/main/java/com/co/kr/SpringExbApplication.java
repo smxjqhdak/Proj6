19d374289e9f6ee2a0586b5e0576dbf860f2bb97
smxjqhdak <jhcha98@naver.com>
2023-04-10 01:28:13 +0900

    first commit

---
+++ src/main/java/com/co/kr/SpringExbApplication.java
@@ -0,0 +1,13 @@
+package com.co.kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExbApplication.class, args);
+	}
+
+}
